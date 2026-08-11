e892e2cd5a0d914952db8e6bbb6e778de1569a0f
muraddavid97-eng <muraddavid97@gmail.com>
2026-08-11 09:25:39 +0300

    new

--- MERN/assignament/Jokes Api/server/config/mongoose.config.js
+++ MERN/assignament/Jokes Api/server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri="mongodb+srv://muraddavid97_db_user:mongodb@cluster0.oumfxkk.mongodb.net/?appName=Cluster0"
+const uri="my mongodb string"
 
 mongoose.connect(uri).then(()=>{
   console.log("database is conecting")
